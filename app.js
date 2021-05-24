16e1f85d581ad2a80980301f11becbfc70604ce1
bdjm <brendandjmoore@gmail.com>
2021-05-24 19:12:48 +1000

    adding npm packages

---
+++ app.js
@@ -0,0 +1,6 @@
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const util = require("util");
+const cTable = require("console.table");
+const logo = require("asciiart-logo");
+const chalk = require("chalk")
