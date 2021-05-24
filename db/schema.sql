841f6cfebfc98530de68b60967aab377ab198fc6
bdjm <brendandjmoore@gmail.com>
2021-05-24 21:51:30 +1000

    creating the employee tracker db

---
+++ db/schema.sql
@@ -0,0 +1,33 @@
+DROP DATABASE IF EXISTS employee_tracker_DB;
+CREATE DATABASE employee_tracker_DB;
+
+USE employee_tracker_DB;
+
+CREATE TABLE department (
+    id INT AUTO_INCREMENT NOT NULL ,
+    department_name VARCHAR(30) NOT NULL,
+    PRIMARY KEY(id),
+);
+
+CREATE TABLE role (
+    id INT AUTO_INCREMENT NOT NULL,
+    title VARCHAR(30) NULL,
+    salary DECIMAL(10, 2) NULL,
+    department_id INT NULL,
+    PRIMARY KEY(id),
+);
+
+CREATE TABLE employees (
+    id INT AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(30) NULL,
+    last_name VARCHAR(30) NULL,
+    role_id INT NOT NULL,
+    manager_id INT NULL,
+    PRIMARY KEY(id),
+);
+
+SELECT * FROM department;
+
+SELECT * FROM role;
+
+SELECT * FROM employees;
