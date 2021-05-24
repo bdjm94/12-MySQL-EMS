0ad027658035f51b10c8a09d8d681442ef4e7bec
bdjm <brendandjmoore@gmail.com>
2021-05-24 19:29:04 +1000

    adding inquirer prompt start choices

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const inquirer = require("inquirer");
 const util = require("util");
 const cTable = require("console.table");
 const logo = require("asciiart-logo");
-const chalk = require("chalk")
+const chalk = require("chalk");
+const { async } = require("rxjs");
 
 function displayLogo() {
     console.log(
@@ -39,3 +40,74 @@ connection.connect((err) => {
     displayLogo();
     startPrompt();
   });
+
+  var startPrompt = async () => {
+      try {
+          var selection = await inquirer.prompt({
+              name: "task",
+              type: "list",
+              message: "Choose a task from the below",
+              choices: [
+                  "Add a new Employee",
+                  "Add a new Role",
+                  "Add a new Department",
+                  "View all Employees",
+                  "View all Roles",
+                  "View all Departments",
+                  "Update Employee Roles",
+                  "Delete Employee",
+                  "Delete Role",
+                  "Exit",
+              ],
+          });
+          switch (selection.task) {
+            case "Add a new Employee":
+                addEmployee();
+                break;
+
+            case "Add a new Role":
+                addRole();
+                break;
+
+            case "Add a new Department":
+                addDepartment();
+                break;
+      
+            case "View all Employees":
+                viewEmployees();
+                break;
+                
+            case "View all Roles":
+                viewRoles();
+                break;      
+
+            case "View all Departments":
+                viewDepartments();
+                break;
+      
+            case "Update Employee Roles":
+                updateEmployeeRoles();
+                break;
+
+            case "Delete Employee":
+                deleteEmployee();
+                break;
+                
+            case "Delete Role":
+                deleteRole();
+                break;    
+      
+            case "Exit":
+              exit();
+              break;
+      
+            default:
+              connection.end();
+          }
+        } catch (err) {
+          console.log(err);
+          console.table(selection.task);
+          startPrompt();
+        }
+      };
+
