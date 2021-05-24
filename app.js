3effaced7a9d3468b0a9ff8b79b1a60f30890421
bdjm <brendandjmoore@gmail.com>
2021-05-24 19:18:14 +1000

    adding sql connection function and starting logo

--- app.js
+++ app.js
@@ -4,3 +4,38 @@ const util = require("util");
 const cTable = require("console.table");
 const logo = require("asciiart-logo");
 const chalk = require("chalk")
+
+function displayLogo() {
+    console.log(
+        logo({
+            name: 'Employee Management System',
+            lineChars: 10,
+            padding: 2,
+            margin: 3,
+            borderColor: 'grey',
+            logoColor: 'green',
+            textColor: 'white',
+        })
+        .render()
+    );
+}
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "employee_tracker_DB",
+  });
+
+connection.query = util.promisify(connection.query);
+
+connection.connect((err) => {
+    if (err) {
+      console.error(chalk.red("Error connecting: " + err.stack));
+      return;
+    }
+    console.log(chalk.magenta("Successfully connected to MySQL server!"));
+    displayLogo();
+    startProgram();
+  });
