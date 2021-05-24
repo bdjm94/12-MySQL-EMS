93c5766236435f4f1eb028955e083743b8e6f2ca
bdjm <brendandjmoore@gmail.com>
2021-05-24 22:05:33 +1000

    adding .env functionality

--- app.js
+++ app.js
@@ -21,11 +21,11 @@ function displayLogo() {
 }
 
 var connection = mysql.createConnection({
-    host: "localhost",
+    host: process.env.DB_HOST,
     port: 3306,
-    user: "root",
-    password: "",
-    database: "employee_tracker_DB",
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
   });
 
 connection.query = util.promisify(connection.query);
